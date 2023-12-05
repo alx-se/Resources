88fe24a9a0003f2744440c6b502dcf68a574dc25
Amanuel Sisay <amanuelsisay.as@gmail.com>
2023-12-05 18:55:11 +0300

    fix: improve implementation for the print_tokens

--- Code/Simple_shell/strtok.c
+++ Code/Simple_shell/strtok.c
@@ -9,10 +9,11 @@
 #include <string.h>
 #include <stdio.h>
 
-void print_tokens(char *str, char *delim)
+void print_tokens(char *str1, char *delim)
 {
 	// str -> Do hard things, delim -> " "
 	// strtok will return NULL if it at the
+	char *str = strdup(str1);
 	char *token = strtok((char *)str, delim);
 	while (token != NULL)
 	{
@@ -25,15 +26,10 @@ void print_tokens(char *str, char *delim)
 
 int main(void)
 {
-	char s[] = "Do hard things";
-	print_tokens (s, " ");
-	char a[] = "Do,hard,things";
-	print_tokens(a, " ,");
-	char b[] = "ABC";
-	print_tokens(b, "ABC");
-	char d[] = "            ";
-	print_tokens(d, " ,");
-	char e[] = "abc       hard       ";
-	print_tokens(e, " ");
+	print_tokens ("Do hard things", " ");
+	print_tokens("Do,hard,things", " ,");
+	print_tokens("ABC", "ABC");
+	print_tokens( "            ", " ,");
+	print_tokens("abc       hard       ", " ");
 	return (0);
 }
